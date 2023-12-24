8c1f2defdf4262c5930fb1661b1d9a5067dc1d7c
Bruhmi <145869045+Bruhmi@users.noreply.github.com>
2023-12-24 16:08:30 +0300

    Update Practice1.cpp

--- Practice1.cpp
+++ Practice1.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 #include <ctime>
 
-union unFl {
-    int UIntVar;
-    float UFlVar;
-};
-
-void idz() {
-    int bitNum;
-    int bit;
-    std::cout << "Введите номер бита: ";
-    std::cin >> bitNum;
-    std::cout << "Введите, на какой бит заменить:";
-    std::cin >> bit;
-}
-
-int Practice1() {
+int main() {
     setlocale(LC_ALL, "ru");
     srand(time(NULL));
     std::cout <<
@@ -24,11 +10,11 @@ int Practice1() {
         "ДДЕЙСТВИЯ:\n"
         "1) Вывести, сколько памяти (в байтах) на вашем компьютере отводится под различные типы данных со спецификаторами и без: "
         "int, short int, long int, float, double, long double, char и bool.\n"
-        "2) Вывести на экран двоичное представление в памяти (все разряды) случайного целого числа.\n"
-        "3) Вывести на экран двоичное представление в памяти (все разряды) случайного числа типа float.\n\n";
-
+        "2) Вывести на экран двоичное представление в памяти (все разряды) целого числа.\n"
+        "3) Вывести на экран двоичное представление в памяти (все разряды) числа типа float.\n"
+        "4) Вывести на экран двоичное представление в памяти (все разряды) числа типа double.\n";
+    int taskNumb;
     for (int stop = 0; stop < 1; stop) {
-        int taskNumb;
         std::cout << "Введите номер действия: ";
         std::cin >> taskNumb;
 
@@ -47,12 +33,12 @@ int Practice1() {
             break;
 
         case 2: {
-            int intVar = rand();
-            std::cout <<
-                "Случайное целое число: " << intVar << "\n" <<
-                "Двоичное представление числа " << intVar << " в памяти: ";
+            int intVar;
+            std::cout << "Введите целое число: ";
+            std::cin >> intVar;
+            std::cout << "Двоичное представление числа " << intVar << " в памяти: ";
             int order = sizeof(int) * 8;
-            unsigned int mask = 1 << (order - 1);
+            unsigned mask = 1 << (order - 1);
 
             for (int i = 0; i < order; ++i) {
                 std::cout << ((intVar & mask) ? 1 : 0);
@@ -71,11 +57,10 @@ int Practice1() {
                 float flVar;
             };
             unsigned int order = 32;
-            flVar = (float)rand() / 7;
+            std::cout << "Введите вещественное число: ";
+            std::cin >> flVar;
             unsigned mask = 1 << (order - 1);
-            std::cout <<
-                "Cлучайное вещественное число: " << flVar << "\n"
-                "Двоичное представление числа " << flVar << " в памяти: ";
+            std::cout << "Двоичное представление числа " << flVar << " в памяти: ";
             for (int i = 1; i <= order; i++) {
                 std::cout << (tool1 & mask ? '1' : '0');
                 mask >>= 1;
@@ -84,16 +69,45 @@ int Practice1() {
                 if (i == 9)
                     std::cout << ' ';
             }
-            std::cout << "\n";
+            std::cout << "\n\n";
             break;
         }
+        case 4: {
+            union {
+                int arr[2];
+                double dbVar;
+            };
+            unsigned int order = 32;
+            std::cout << "Введите вещественное число: ";
+            std::cin >> dbVar;
+            unsigned mask = 1 << (order - 1);
+            std::cout << "Двоичное представление числа " << dbVar << " в памяти: ";
+            for (int i = 1; i <= order; i++) {
+                std::cout << (arr[1] & mask ? '1' : '0');
+                mask >>= 1;
+                if (i == 1)
+                    std::cout << ' ';
+                if (i == 9)
+                    std::cout << ' ';
+            }
+            std::cout << " ";
 
-        case 0: {
+            mask = 1 << (order - 1);
+            for (int i = 1; i <= order; i++) {
+                std::cout << (arr[0] & mask ? '1' : '0');
+                mask >>= 1;
+                if (i == 1)
+                    std::cout << ' ';
+                if (i == 9)
+                    std::cout << ' ';
+            }
+            std::cout << "\n\n";
+            break;
+        }
+        case 0:
             stop = 1;
             break;
         }
-        }
-        return 0;
     }
+    return 0;
 }
-
